a9729ab128139ded009e1a7a6a795048df0560c6
dev-Knight-fire <locklee0418@gmail.com>
2025-08-08 22:23:42 -0800

    modified cooper index

--- Components/Shared/Navbar/Navbar.js
+++ Components/Shared/Navbar/Navbar.js
@@ -64,7 +64,7 @@ const Navbar = () => {
       link: "/properties",
     },
     {
-      item: "Cooper",
+      item: "Cooper Index",
       link: "/cooper",
     },
     {
@@ -204,7 +204,7 @@ const Navbar = () => {
                   href="/cooper"
                   className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
-                  Cooper
+                  Cooper Index
                 </Link>
               </li>
               <li>
